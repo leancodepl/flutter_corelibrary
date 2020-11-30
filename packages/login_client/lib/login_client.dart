5156e7bee2d7bcad4e86b038a55f53189c3fa6da
Albert Wolszon <albert.wolszon@leancode.pl>
2020-11-30 13:40:43 +0100

    Rename custom_strategy to custom_grant_strategy

--- packages/login_client/lib/login_client.dart
+++ packages/login_client/lib/login_client.dart
@@ -6,7 +6,7 @@ export 'src/login_client.dart';
 export 'src/oauth_settings.dart';
 export 'src/strategies/authorization_strategy.dart';
 export 'src/strategies/client_credentials_strategy.dart';
-export 'src/strategies/custom_strategy.dart';
+export 'src/strategies/custom_grant_strategy.dart';
 export 'src/strategies/raw_credentials_strategy.dart';
 export 'src/strategies/resource_owner_password_strategy.dart';
 export 'src/strategies/sms_token_strategy.dart';
