7264172b4c143753865fd260cb2355a23dfe4138
Albert221 <w.albert221@gmail.com>
2020-12-02 09:43:30 +0100

    Use sync futures for InMemoryCredentialsStorage

--- packages/login_client/lib/src/credentials_storage/in_memory_credentials_storage.dart
+++ packages/login_client/lib/src/credentials_storage/in_memory_credentials_storage.dart
@@ -21,17 +21,17 @@ class InMemoryCredentialsStorage extends CredentialsStorage {
   oauth2.Credentials _credentials;
 
   @override
-  Future<oauth2.Credentials> read() async {
-    return _credentials;
+  Future<oauth2.Credentials> read() {
+    return Future.value(_credentials);
   }
 
   @override
-  Future<void> save(oauth2.Credentials credentials) async {
-    _credentials = credentials;
+  Future<void> save(oauth2.Credentials credentials) {
+    return Future.sync(() => _credentials = credentials);
   }
 
   @override
-  Future<void> clear() async {
-    _credentials = null;
+  Future<void> clear() {
+    return Future.sync(() => _credentials = null);
   }
 }
