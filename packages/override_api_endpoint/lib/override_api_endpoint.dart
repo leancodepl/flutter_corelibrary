8af154ab19a5496655a67734fb4f36fe56d361e7
lukaszgarstecki <50754302+lukaszgarstecki@users.noreply.github.com>
2021-08-13 14:04:17 +0200

    Introduce override_api_endpoint package (#24)
    * Add override_api_endpoint package
    
    * Fix README
    
    * Add overrideApiEndpoint parameters info
    
    * Fix analysis_options
    
    * Fix package description
    
    * Remove unnecessary dev dependencies
    
    * Wrap function with mockable class; Remove unilinks dependency
    
    * Add readme documentation
    
    * Add configurable apiEndpoint key
    
    * Make all params named
    
    * Fix typo
    
    fix typo
    
    Co-authored-by: Albert Wolszon <albert.wolszon@leancode.pl>
    
    * Fix typo; Remove typedef
    
    Co-authored-by: Mateusz Pietras <mateusz.pietras@leancode.pl>
    Co-authored-by: XezolPL <42344554+Xezolpl@users.noreply.github.com>
    Co-authored-by: Albert Wolszon <albert.wolszon@leancode.pl>

---
+++ packages/override_api_endpoint/lib/override_api_endpoint.dart
@@ -0,0 +1,3 @@
+library override_api_endpoint;
+
+export 'src/override_api_endpoint.dart';
---
+++ packages/override_api_endpoint/lib/src/override_api_endpoint.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Overrides and persists API endpoint for the test environment
+///
+/// The `deeplinkOverrideSegment` is the part of deeplink that uniquely
+/// identifies deeplink that is used to override API endpoint
+/// eg. `override` in `app://app/override?apiAddress=https%3A%2F%2Fexample.com`
+///
+/// The `deeplinkQueryParameter` is the query parameter of the override API
+/// endpoint deeplink that contains url encoded API endpoint to be used
+/// eg. `apiAddress` in `app://app/override?apiAddress=https%3A%2F%2Fexample.com`
+///
+/// The `defaultEndpoint` is fallback url that should be used if app does not
+/// have any endpoint introduced via deeplink or if `deeplinkQueryParameter` is
+/// not provided
+Future<Uri> overrideApiEndpoint({
+  required SharedPreferences sharedPreferences,
+  required FutureOr<Uri?> Function() getInitialUri,
+  required String deeplinkOverrideSegment,
+  required String deeplinkQueryParameter,
+  required Uri defaultEndpoint,
+  String apiEndpointKey = 'apiAddress',
+}) async {
+  var apiEndpoint = defaultEndpoint;
+
+  if (sharedPreferences.containsKey(apiEndpointKey)) {
+    apiEndpoint = Uri.parse(sharedPreferences.getString(apiEndpointKey)!);
+  }
+
+  final initial = await getInitialUri();
+
+  if (initial != null && initial.path.contains(deeplinkOverrideSegment)) {
+    final endpointFromDeeplink =
+        initial.queryParameters[deeplinkQueryParameter];
+    if (endpointFromDeeplink?.isEmpty ?? true) {
+      sharedPreferences.remove(apiEndpointKey);
+      apiEndpoint = defaultEndpoint;
+    } else {
+      apiEndpoint = Uri.parse(endpointFromDeeplink!);
+      sharedPreferences.setString(apiEndpointKey, apiEndpoint.toString());
+    }
+  }
+
+  return apiEndpoint;
+}
