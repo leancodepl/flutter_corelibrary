0aa9b190b4c826c9a6e4d5606438cb0d008322d9
Bartek Pacia <barpac02@gmail.com>
2022-04-29 00:59:46 +0200

    use_post_frame_effect

---
+++ packages/leancode_hooks/lib/src/use_post_frame_effect.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+/// Registers [effect] to be run in
+/// WidgetsBinding.instance.addPostFrameCallback.
+void usePostFrameEffect(VoidCallback effect, [List<Object>? keys]) {
+  useEffect(
+    () {
+      WidgetsBinding.instance!.addPostFrameCallback((_) => effect());
+      return null;
+    },
+    keys,
+  );
+}
