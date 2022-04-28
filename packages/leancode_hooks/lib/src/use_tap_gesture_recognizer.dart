98ead28f2ae60a0c181a61afb706e0f3f3b98f45
Bartek Pacia <barpac02@gmail.com>
2022-04-29 01:02:49 +0200

    add use_tap_gesture_recognizer

---
+++ packages/leancode_hooks/lib/src/use_tap_gesture_recognizer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+export 'package:flutter/gestures.dart' show TapGestureRecognizer;
+
+TapGestureRecognizer useTapGestureRecognizer(
+  TapGestureRecognizer Function() builder, [
+  List<Object?> keys = const [],
+]) {
+  final tapGestureRecognizer = useMemoized(builder, keys);
+
+  useEffect(() => tapGestureRecognizer.dispose, [tapGestureRecognizer]);
+
+  return tapGestureRecognizer;
+}
