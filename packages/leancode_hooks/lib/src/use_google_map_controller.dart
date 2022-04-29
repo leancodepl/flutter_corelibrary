4ac456ece634470bd9afeeacc94ac56ef67f7265
Bartek Pacia <barpac02@gmail.com>
2022-04-29 15:31:51 +0200

    add experimental useGoogleMapController

--- packages/leancode_hooks/lib/src/use_google_map_controller.dart
+++ packages/leancode_hooks/lib/src/use_google_map_controller.dart
@@ -1,9 +1,38 @@
 import 'dart:async';
 
+import 'package:discover_rudy/resources/resources.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+// Example copied from Bartek's project.
+
 Completer<GoogleMapController> useGoogleMapController() {
+  final context = useContext();
+  final brightness = MediaQuery.of(context).platformBrightness;
   final mapCompleter = useMemoized(Completer<GoogleMapController>.new);
+
+  useEffect(
+    () {
+      mapCompleter.future.then((controller) async {
+        final lightStyle = await rootBundle.loadString(Assets.mapStyles.light);
+        final darkStyle = await rootBundle.loadString(Assets.mapStyles.dark);
+
+        switch (brightness) {
+          case Brightness.light:
+            await controller.setMapStyle(lightStyle);
+            break;
+          case Brightness.dark:
+            await controller.setMapStyle(darkStyle);
+            break;
+        }
+      });
+
+      return null;
+    },
+    [mapCompleter, brightness],
+  );
+
   return mapCompleter;
 }
