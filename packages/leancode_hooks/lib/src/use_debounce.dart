e93a2fc799c2e2aff958a6ad1914c3c44eb8e5be
Bartek Pacia <barpac02@gmail.com>
2022-04-30 00:16:10 +0200

    make useDebounce not value-oriented

--- packages/leancode_hooks/lib/src/use_debounce.dart
+++ packages/leancode_hooks/lib/src/use_debounce.dart
@@ -1,17 +1,33 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-T useDebounce<T>(T value, Duration duration) {
-  final state = useState(value);
+/// Signature of function that is returned by [useDebounce].
+typedef DebouncedCallback = void Function(VoidCallback callback);
 
-  useEffect(
-    () {
-      final timer = Timer(duration, () => state.value = value);
-      return timer.cancel;
-    },
-    [value, duration],
-  );
+/// Returns a function whose invocation will be delayed by [duration].
+///
+/// When the returned function is called again, the previous (scheduled)
+/// invocation is canceled.
+///
+/// See also:
+///
+///  * <https://rxmarbles.com/#debounce>, which this hook implements.
+DebouncedCallback useDebounce(Duration duration) {
+  final debouncer = useMemoized(() => _Debouncer(duration), [duration]);
+  return debouncer.run;
+}
 
-  return state.value;
+class _Debouncer {
+  _Debouncer(this.duration);
+
+  final Duration duration;
+
+  Timer? _timer;
+
+  void run(VoidCallback callback) {
+    _timer?.cancel();
+    _timer = Timer(duration, callback);
+  }
 }
