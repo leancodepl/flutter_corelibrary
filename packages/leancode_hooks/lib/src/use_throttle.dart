0eee51b9e2b0218cbb1be96fdba379af45935c7e
Bartek Pacia <barpac02@gmail.com>
2022-04-29 23:25:51 +0200

    rename useCooldown to useThrottle

---
+++ packages/leancode_hooks/lib/src/use_throttle.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+/// Signature for function that is returned from [useThrottle].
+typedef ThrottledFunc = void Function(VoidCallback callback);
+
+/// Returns a function that can be called only once per [duration].
+///
+/// When [ThrottledFunc] that this hook returns is called, a cooldown of
+/// [duration] is started. During it, any calls to [ThrottledFunc] are ignored.
+ThrottledFunc useThrottle(
+  Duration duration, [
+  List<Object?> keys = const <Object>[],
+]) {
+  final throttler = useMemoized(() => _Throttler(duration), keys);
+  return throttler.run;
+}
+
+class _Throttler {
+  _Throttler(this.duration);
+
+  final Duration duration;
+
+  bool _locked = false;
+
+  Future<void> run(VoidCallback fun) async {
+    if (!_locked) {
+      _locked = true;
+      fun();
+      await Future<void>.delayed(duration);
+      _locked = false;
+    }
+  }
+}
