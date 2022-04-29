e7005e601961a0f7ec456111ae47b836d33188fc
Bartek Pacia <barpac02@gmail.com>
2022-04-30 00:27:12 +0200

    update docs for useThrottle

--- packages/leancode_hooks/lib/src/use_throttle.dart
+++ packages/leancode_hooks/lib/src/use_throttle.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-/// Signature for function that is returned from [useThrottle].
-typedef ThrottledFunc = void Function(VoidCallback callback);
+/// Signature of function that is returned by [useThrottle].
+typedef ThrottledCallback = void Function(VoidCallback callback);
 
 /// Returns a function that can be called only once per [duration].
 ///
-/// When [ThrottledFunc] that this hook returns is called, a cooldown of
-/// [duration] is started. During it, any calls to [ThrottledFunc] are ignored.
-ThrottledFunc useThrottle(Duration duration) {
+/// When the returned function is called, a cooldown of [duration] is started.
+/// During its duration, any calls to the returned function are ignored.
+///
+/// See also:
+///
+/// * <https://rxmarbles.com/#throttle>, which this hook implements.
+ThrottledCallback useThrottle(Duration duration) {
   final throttler = useMemoized(() => _Throttler(duration), [duration]);
   return throttler.run;
 }
