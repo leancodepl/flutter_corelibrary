397650c49bd8ad4ad33d52a73dc79a350d01943a
Bartek Pacia <barpac02@gmail.com>
2022-04-29 23:19:40 +0200

    useGoogleMapController: load map styles only once

--- packages/leancode_hooks/lib/src/use_google_map_controller.dart
+++ packages/leancode_hooks/lib/src/use_google_map_controller.dart
@@ -1,37 +1,56 @@
 import 'dart:async';
 
-import 'package:discover_rudy/resources/resources.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-// Example copied from Bartek's project.
-
-Completer<GoogleMapController> useGoogleMapController() {
+/// Returns a dark-mode aware [GoogleMapController].
+Completer<GoogleMapController> useGoogleMapController({
+  required String lightStylePath,
+  required String darkStylePath,
+}) {
   final context = useContext();
   final brightness = MediaQuery.of(context).platformBrightness;
   final mapCompleter = useMemoized(Completer<GoogleMapController>.new);
 
+  final lightStyleFuture = useMemoized(
+    () => rootBundle.loadString(lightStylePath),
+  );
+
+  final darkStyleFuture = useMemoized(
+    () => rootBundle.loadString(darkStylePath),
+  );
+
+  final lightStyleSnapshot = useFuture(lightStyleFuture);
+  final darkStyleSnapshot = useFuture(darkStyleFuture);
+
   useEffect(
     () {
-      mapCompleter.future.then((controller) async {
-        final lightStyle = await rootBundle.loadString(Assets.mapStyles.light);
-        final darkStyle = await rootBundle.loadString(Assets.mapStyles.dark);
+      var ready = false;
+      if (lightStyleSnapshot.hasData && darkStyleSnapshot.hasData) {
+        ready = true;
+      }
 
+      if (!ready) {
+        return null;
+      }
+
+      // register callback only when ready
+      mapCompleter.future.then((controller) async {
         switch (brightness) {
           case Brightness.light:
-            await controller.setMapStyle(lightStyle);
+            await controller.setMapStyle(lightStyleSnapshot.data);
             break;
           case Brightness.dark:
-            await controller.setMapStyle(darkStyle);
+            await controller.setMapStyle(darkStyleSnapshot.data);
             break;
         }
       });
 
       return null;
     },
-    [mapCompleter, brightness],
+    [mapCompleter, brightness, lightStyleSnapshot.data, darkStyleSnapshot.data],
   );
 
   return mapCompleter;
