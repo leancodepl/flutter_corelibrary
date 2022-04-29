ccbeb841092d988e04d41f74c44d8a212879c076
Bartek Pacia <barpac02@gmail.com>
2022-04-30 01:00:30 +0200

    improe useStreamListener hook

--- packages/leancode_hooks/lib/src/use_stream_listener.dart
+++ packages/leancode_hooks/lib/src/use_stream_listener.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-/// Listens to [stream] and calls [callback] on new data.
+/// Listens to [stream] and calls [onData] on new data.
+///
+/// See also:
+///
+/// * [Stream.listen] for information about [onError], [onDone], and
+///   [cancelOnError].
 void useStreamListener<T>(
   Stream<T> stream,
-  ValueChanged<T> callback, [
+  ValueChanged<T> onData, {
+  Function? onError,
+  void Function()? onDone,
+  bool? cancelOnError,
   List<Object?> keys = const [],
-]) {
+}) {
   useEffect(
     () {
-      final subscription = stream.listen(callback);
+      final subscription = stream.listen(
+        onData,
+        onError: onError,
+        onDone: onDone,
+        cancelOnError: cancelOnError,
+      );
 
       return subscription.cancel;
     },
