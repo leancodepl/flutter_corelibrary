2928605fa294160d69be223cfc0344ba32b7770f
Bartek Pacia <barpac02@gmail.com>
2022-04-30 12:55:57 +0200

    make useBloc return BlocBase (so either Cubit or Bloc)

--- packages/leancode_hooks/lib/src/use_bloc.dart
+++ packages/leancode_hooks/lib/src/use_bloc.dart
@@ -1,26 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-/// Provides a [Bloc] without having to use BlocProvider.
-B useBloc<B extends Bloc<S, E>, S, E>(
+/// Provides a [Cubit] or a [Bloc] that is automatically disposed without having
+/// to use BlocProvider.
+B useBloc<B extends BlocBase<Object?>>(
   B Function() create, [
-  List<Object> keys = const [],
+  List<Object?> keys = const [],
 ]) {
   final bloc = useMemoized(create, keys);
 
   useEffect(() => bloc.close, [bloc]);
 
   return bloc;
-}
-
-/// Provides a [Cubit] without having to use BlocProvider.
-C useCubit<C extends Cubit<S>, S>(
-  C Function() create, [
-  List<Object> keys = const [],
-]) {
-  final cubit = useMemoized(create, keys);
-
-  useEffect(() => cubit.close, [cubit]);
-
-  return cubit;
 }
