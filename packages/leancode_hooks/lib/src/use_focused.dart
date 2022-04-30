59f366127f6723dea82e7c311e9b236ca032d4b3
Bartek Pacia <barpac02@gmail.com>
2022-04-30 13:02:28 +0200

    refactor useFocused to use useListenable under the hood

--- packages/leancode_hooks/lib/src/use_focused.dart
+++ packages/leancode_hooks/lib/src/use_focused.dart
@@ -2,19 +2,5 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 bool useFocused(FocusNode focusNode) {
-  final focued = useState<bool>(false);
-
-  useEffect(
-    () {
-      void listener() {
-        focued.value = focusNode.hasFocus;
-      }
-
-      focusNode.addListener(listener);
-      return () => focusNode.removeListener(listener);
-    },
-    [focusNode],
-  );
-
-  return focued.value;
+  return useListenable(focusNode).hasFocus;
 }
