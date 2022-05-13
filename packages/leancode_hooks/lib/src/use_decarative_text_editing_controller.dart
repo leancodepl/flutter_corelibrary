91986a438690a0c6b44cbbf12f930ff5d74ff823
Bartek Pacia <barpac02@gmail.com>
2022-05-13 16:07:25 +0200

    docs fix

--- packages/leancode_hooks/lib/src/use_decarative_text_editing_controller.dart
+++ packages/leancode_hooks/lib/src/use_decarative_text_editing_controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 /// A wrapper around [useTextEditingController] that updates
-/// [TextEditingController.text] when it changes.
+/// [TextEditingController.text] when the text changes.
 ///
 /// Does not handle some edge cases with the current cursor/selection.
 TextEditingController useDeclarativeTextEditingController({
