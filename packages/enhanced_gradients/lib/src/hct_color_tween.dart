79337b746b02e3039d59888b9ec3a95ff0b78bb6
Tomasz Koter <tomasz.koter@leancode.pl>
2023-06-26 16:21:19 +0200

    refactor(enhanced_gradients): `lerpHct` code style

--- packages/enhanced_gradients/lib/src/hct_color_tween.dart
+++ packages/enhanced_gradients/lib/src/hct_color_tween.dart
@@ -24,22 +24,24 @@ Color? lerpHct(Color? colorA, Color? colorB, double t) =>
       (null, null) => null,
       (final colorA?, null) => _scaleAlpha(colorA, 1 - t),
       (null, final colorB?) => _scaleAlpha(colorB, t),
-      (final colorA?, final colorB?) => () {
-          final beginHct = Hct.fromInt(colorA.value);
-          final endHct = Hct.fromInt(colorB.value);
-
-          final opacity = lerpDouble(colorA.opacity, colorB.opacity, t)!;
-          final hue = lerpDegrees(beginHct.hue, endHct.hue, t)!;
-          final chroma = lerpDouble(beginHct.chroma, endHct.chroma, t)!;
-          final tone = lerpDouble(beginHct.tone, endHct.tone, t)!;
-
-          return Color(
-            // This is a slight optimization over calling
-            // `Hct.from(hue, chroma, tone).toInt()`
-            HctSolver.solveToInt(hue, chroma, tone),
-          ).withOpacity(opacity);
-        }(),
+      (final colorA?, final colorB?) => _lerpHctNonNullable(colorA, colorB, t),
     };
+
+Color _lerpHctNonNullable(Color colorA, Color colorB, double t) {
+  final beginHct = Hct.fromInt(colorA.value);
+  final endHct = Hct.fromInt(colorB.value);
+
+  final opacity = lerpDouble(colorA.opacity, colorB.opacity, t)!;
+  final hue = lerpDegrees(beginHct.hue, endHct.hue, t)!;
+  final chroma = lerpDouble(beginHct.chroma, endHct.chroma, t)!;
+  final tone = lerpDouble(beginHct.tone, endHct.tone, t)!;
+
+  return Color(
+    // This is a slight optimization over calling
+    // `Hct.from(hue, chroma, tone).toInt()`
+    HctSolver.solveToInt(hue, chroma, tone),
+  ).withOpacity(opacity);
+}
 
 Color _scaleAlpha(Color a, double factor) {
   return a.withAlpha((a.alpha * factor).round().clamp(0, 255));
