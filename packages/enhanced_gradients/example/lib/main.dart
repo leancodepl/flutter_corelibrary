8f5523fb21b84c7b1dfc99a11bebac892c83a1fa
Tomasz Koter <tomasz.koter@leancode.pl>
2023-06-28 18:40:56 +0200

    chore(enhanced_gradients): re-add example app
    This reverts commit cf9783c607f3df22f436fad9b0e0de57bdc98891.

---
+++ packages/enhanced_gradients/example/lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:math';
+
+import 'package:enhanced_gradients/enhanced_gradients.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<Color> _colors = [];
+  List<double>? _stops;
+
+  final _random = Random(42);
+
+  void _randomizeGradient() {
+    final length = _random.nextInt(2) + 2;
+    setState(() {
+      _colors = List.generate(
+        length,
+        (index) => Color.fromARGB(
+          _random.nextInt(256),
+          _random.nextInt(256),
+          _random.nextInt(256),
+          _random.nextInt(256),
+        ),
+      );
+      final useStops = _random.nextDouble() > 0.2;
+      if (useStops) {
+        _stops = List<double>.generate(length, (index) => _random.nextDouble())
+          ..sort();
+        _stops![0] = 0;
+        _stops![length - 1] = 1;
+      } else {
+        _stops = null;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _randomizeGradient();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final gradient = LinearGradient(colors: _colors, stops: _stops);
+
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: _randomizeGradient,
+          child: const Icon(Icons.shuffle),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _GradientPreview(
+              title: 'Regular LinearGradient',
+              gradient: gradient,
+            ),
+            const SizedBox(height: 16),
+            _GradientPreview(
+              title: 'Enhanced LinearGradient',
+              gradient: gradient.enhanced(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _GradientPreview extends StatelessWidget {
+  const _GradientPreview({
+    required this.title,
+    required this.gradient,
+  });
+
+  final String title;
+  final Gradient gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(title),
+        const SizedBox(height: 8),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            gradient: gradient,
+          ),
+          child: const SizedBox(
+            width: double.infinity,
+            height: 48,
+          ),
+        ),
+      ],
+    );
+  }
+}
