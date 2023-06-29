4d3903f98518451eca85272a261ba8d0cba538ab
Kamil Sztandur <kamil.sztandur@vp.pl>
2023-06-29 09:17:03 +0200

    Organize imports

--- packages/leancode_hooks/lib/leancode_hooks.dart
+++ packages/leancode_hooks/lib/leancode_hooks.dart
@@ -2,9 +2,9 @@ library leancode_hooks;
 
 export 'package:flutter_hooks/flutter_hooks.dart';
 
+export 'src/use_bloc.dart';
 export 'src/use_bloc_listener.dart';
 export 'src/use_bloc_state.dart';
-export 'src/use_bloc.dart';
 export 'src/use_debounce.dart';
 export 'src/use_decarative_text_editing_controller.dart';
 export 'src/use_focused.dart';
