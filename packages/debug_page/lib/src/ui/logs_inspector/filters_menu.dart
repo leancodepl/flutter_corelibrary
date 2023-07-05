94128c8d75824dc5f2dd9e47e2672500c4cfc56b
gogolon <qbidev@protonmail.com>
2023-07-05 16:09:16 +0200

    Add filters menu

---
+++ packages/debug_page/lib/src/ui/logs_inspector/filters_menu.dart
@@ -0,0 +1,106 @@
+import 'package:debug_page/src/request_log.dart';
+import 'package:flutter/material.dart';
+
+enum SearchType {
+  endpoints,
+  body,
+  all,
+}
+
+class FiltersMenu extends StatefulWidget implements PreferredSizeWidget {
+  const FiltersMenu({super.key});
+
+  static const height = 100.0;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FiltersMenuState();
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(height);
+}
+
+class _FiltersMenuState extends State<FiltersMenu> {
+  RequestStatus? requestStatus;
+  SearchType? searchType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _LabeledDropdown<RequestStatus>(
+            title: const Text('Rodzaj'),
+            onChanged: (value) => setState(() => requestStatus = value),
+            options: const [
+              (value: RequestStatus.success, label: '200'),
+              (value: RequestStatus.redirect, label: '300'),
+              (value: RequestStatus.error, label: '400'),
+            ],
+          ),
+          _LabeledDropdown<SearchType>(
+            title: const Text('Szukaj po'),
+            onChanged: (value) => setState(() => searchType = value),
+            options: const [
+              (value: SearchType.endpoints, label: 'Endpointy'),
+              (value: SearchType.body, label: 'Body'),
+              (value: SearchType.all, label: 'All'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _LabeledDropdown<T> extends StatefulWidget {
+  const _LabeledDropdown({
+    required this.title,
+    required this.options,
+    required this.onChanged,
+  });
+
+  final Widget title;
+  final List<({T value, String label})> options;
+  final ValueChanged<T?> onChanged;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LabeledDropdownState<T>();
+  }
+}
+
+class _LabeledDropdownState<T> extends State<_LabeledDropdown<T>> {
+  T? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        widget.title,
+        const SizedBox(width: 20),
+        Expanded(
+          child: DropdownButton<T>(
+            isExpanded: true,
+            value: selectedValue,
+            onChanged: (value) {
+              setState(() => selectedValue = value);
+              widget.onChanged(value);
+            },
+            items: widget.options
+                .map(
+                  (item) => DropdownMenuItem<T>(
+                    value: item.value,
+                    child: Text(item.label),
+                  ),
+                )
+                .toList(),
+          ),
+        )
+      ],
+    );
+  }
+}
--- packages/debug_page/lib/src/ui/logs_inspector/logs_inspector.dart
+++ packages/debug_page/lib/src/ui/logs_inspector/logs_inspector.dart
@@ -1,11 +1,46 @@
 import 'package:debug_page/debug_page.dart';
 import 'package:debug_page/src/logger_listener.dart';
+import 'package:debug_page/src/ui/logs_inspector/filters_menu.dart';
 import 'package:debug_page/src/ui/logs_inspector/logger/logs_inspector_logger_tab.dart';
 import 'package:debug_page/src/ui/logs_inspector/requests/logs_inspector_requests_tab.dart';
 import 'package:debug_page/src/ui/typography.dart';
 import 'package:flutter/material.dart';
 
-class LogsInspector extends StatelessWidget {
+class _Bottom extends StatelessWidget implements PreferredSizeWidget {
+  _Bottom({required this.expanded});
+
+  final bool expanded;
+
+  final _tabBar = TabBar(
+    labelStyle: DebugPageTypography.medium,
+    tabs: const [
+      Text('Requests'),
+      Text('Logs'),
+    ],
+  );
+
+  @override
+  Size get preferredSize => expanded
+      ? _tabBar.preferredSize + const Offset(0, FiltersMenu.height)
+      : Size.zero;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (expanded)
+          const SizedBox(
+            height: FiltersMenu.height,
+            child: FiltersMenu(),
+          ),
+        _tabBar,
+      ],
+    );
+  }
+}
+
+class LogsInspector extends StatefulWidget {
   const LogsInspector({
     super.key,
     required LoggingHttpClient loggingHttpClient,
@@ -17,24 +52,35 @@ class LogsInspector extends StatelessWidget {
   final LoggerListener _loggerListener;
 
   @override
+  State<StatefulWidget> createState() {
+    return _LogsInspectorState();
+  }
+}
+
+class _LogsInspectorState extends State<LogsInspector> {
+  bool showFilters = false;
+
+  @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Logs inspector'),
-          bottom: TabBar(
-            labelStyle: DebugPageTypography.medium,
-            tabs: const [
-              Text('Requests'),
-              Text('Logs'),
-            ],
-          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.tune),
+              onPressed: () => setState(() => showFilters = !showFilters),
+            )
+          ],
+          bottom: _Bottom(expanded: showFilters),
         ),
         body: TabBarView(
           children: [
-            LogsInspectorRequestsTab(loggingHttpClient: _loggingHttpClient),
-            LogsInspectorLoggerTab(loggerListener: _loggerListener),
+            LogsInspectorRequestsTab(
+              loggingHttpClient: widget._loggingHttpClient,
+            ),
+            LogsInspectorLoggerTab(loggerListener: widget._loggerListener),
           ],
         ),
       ),
