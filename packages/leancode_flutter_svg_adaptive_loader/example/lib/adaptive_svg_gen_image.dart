45b5180cb3dbc8846ebdc0bb7083e3706629c70d
Mateusz Pietras <42344554+Xezolpl@users.noreply.github.com>
2023-08-09 22:54:59 +0200

    Add example for LeancodeFlutterSvgAdaptiveLoader (#127)
    * Add example for LeancodeFlutterSvgAdaptiveLoader
    
    * Remove wrongly placed dependency
    
    ---------
    
    Co-authored-by: Mateusz Pietras <mateusz.pietras@leancode.pl>

---
+++ packages/leancode_flutter_svg_adaptive_loader/example/lib/adaptive_svg_gen_image.dart
@@ -0,0 +1,43 @@
+import 'package:example/gen/assets.gen.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:leancode_flutter_svg_adaptive_loader/leancode_flutter_svg_adaptive_loader.dart';
+
+extension AdaptiveSvgGenImage on SvgGenImage {
+  SvgPicture adaptiveSvg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+  }) {
+    return SvgPicture(
+      LeancodeFlutterSvgAdaptiveLoader(
+        path,
+        assetBundle: bundle,
+        packageName: package,
+      ),
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter: colorFilter,
+      clipBehavior: clipBehavior,
+    );
+  }
+}
---
+++ packages/leancode_flutter_svg_adaptive_loader/example/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:example/adaptive_svg_gen_image.dart';
+import 'package:example/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:leancode_flutter_svg_adaptive_loader/leancode_flutter_svg_adaptive_loader.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+                'Assets created by SvgPicture(LeancodeFlutterSvgAdaptiveLoader("asset_path"))'),
+            const SizedBox(height: 8),
+            const Row(
+              children: [
+                Expanded(
+                  child: _SvgPictureWithTitle(
+                    text: 'XML-based foo',
+                    svgPicture: SvgPicture(
+                      LeancodeFlutterSvgAdaptiveLoader('assets/foo.svg'),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: _SvgPictureWithTitle(
+                    text: 'Binary compiled foo',
+                    svgPicture: SvgPicture(
+                      LeancodeFlutterSvgAdaptiveLoader(
+                        'assets/foo_compiled.svg',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(),
+            const Text('Assets created by SvgGenImage.adaptiveSvg()'),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: _SvgPictureWithTitle(
+                    text: 'XML-based foo',
+                    svgPicture: Assets.foo.adaptiveSvg(),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: _SvgPictureWithTitle(
+                    text: 'Binary compiled foo',
+                    svgPicture: Assets.fooCompiled.adaptiveSvg(),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SvgPictureWithTitle extends StatelessWidget {
+  const _SvgPictureWithTitle({
+    required this.text,
+    required this.svgPicture,
+  });
+
+  final String text;
+  final Widget svgPicture;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(text),
+        const SizedBox(height: 8),
+        svgPicture,
+      ],
+    );
+  }
+}
