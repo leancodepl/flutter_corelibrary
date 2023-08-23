74c59a89d727cd696556f8a7eb87e4f2803430c0
Jakub Tarkowski <jtarkowski1227@gmail.com>
2023-08-23 22:24:28 +0200

    Add initial library docs

--- packages/cqrs_wrapper/lib/cqrs_wrapper.dart
+++ packages/cqrs_wrapper/lib/cqrs_wrapper.dart
@@ -11,3 +11,52 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+/// A library providing a convenient way of handling CQRS errors.
+///
+/// Example:
+///
+/// ```dart
+/// final apiUri = Uri.parse('https://budget.manager/api/');
+/// final logger = Logger('BudgetManager')
+///
+/// final cqrs = Cqrs(
+///   loginClient,
+///   apiUri,
+/// );
+///
+/// final cqrsWrapper = CqrsWrapper(
+///   cqrs: cqrs,
+///   logger: logger,
+/// );
+///
+/// // Fetching first page of the transactions with error handling
+/// final result = await cqrsWrapper.noThrowGet(AllTransactions(page: 1));
+///
+/// if (result.isSuccesful) {
+///   print(result.data);
+/// } else if (result.isFailure) {
+///   print(result.error);
+/// }
+///
+/// // Adding a new transaction and
+/// final result = await cqrsWrapper.noThrowRun(
+///   AddTransaction(
+///     amount: 100,
+///     title: 'Groceries',
+///   ),
+/// );
+///
+/// if (result.isSuccess) {
+///   print('Transaction added succefully');
+/// } else if (result.isInvalid) {
+///   print('Invalid data passed');
+/// } else if (result.isFailure) {
+///   print('Something failed');
+/// }
+/// ```
+library;
+
+export 'src/cqrs_error.dart';
+export 'src/cqrs_result.dart';
+export 'src/cqrs_wrapper.dart';
