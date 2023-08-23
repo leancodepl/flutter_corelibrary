36e081699c35ba86c1ea7b0b3c4d64779ef7ba91
Jakub Tarkowski <jtarkowski1227@gmail.com>
2023-08-23 19:37:19 +0200

    Simplify Cqrs errors to just enums

--- packages/cqrs_wrapper/lib/src/cqrs_error.dart
+++ packages/cqrs_wrapper/lib/src/cqrs_error.dart
@@ -1,51 +1,16 @@
 // ignore_for_file: public_member_api_docs
 
-import 'package:cqrs/cqrs.dart';
+enum CqrsQueryError {
+  unknown,
+  network,
+  forbiddenAccess,
+  authentication,
+}
 
-enum CqrsCommandErrorType {
+enum CqrsCommandError {
+  unknown,
+  network,
+  forbiddenAccess,
+  authentication,
   validation,
-  unknown,
-  network,
-  forbiddenAccess,
-  authentication,
-}
-
-enum CqrsQueryErrorType {
-  unknown,
-  network,
-  forbiddenAccess,
-  authentication,
-}
-
-final class CqrsCommandError {
-  CqrsCommandError.validation(List<ValidationError> validationErrors)
-      : errorType = CqrsCommandErrorType.validation,
-        validationErrors = List.unmodifiable(validationErrors);
-
-  CqrsCommandError.unknown()
-      : errorType = CqrsCommandErrorType.unknown,
-        validationErrors = const [];
-
-  CqrsCommandError.network()
-      : errorType = CqrsCommandErrorType.network,
-        validationErrors = const [];
-
-  CqrsCommandError.forbiddenAccess()
-      : errorType = CqrsCommandErrorType.forbiddenAccess,
-        validationErrors = const [];
-
-  CqrsCommandError.authentication()
-      : errorType = CqrsCommandErrorType.authentication,
-        validationErrors = const [];
-
-  final List<ValidationError> validationErrors;
-  final CqrsCommandErrorType errorType;
-
-  bool get isValidationError => errorType == CqrsCommandErrorType.validation;
-}
-
-final class CqrsQueryError {
-  const CqrsQueryError(this.errorType);
-
-  final CqrsQueryErrorType errorType;
 }
