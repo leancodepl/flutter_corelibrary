eddce3eff93fce1fe37bc0d0f4ac60b1cc3d4587
Jakub Tarkowski <jtarkowski1227@gmail.com>
2023-08-23 13:42:07 +0200

    Add CqrsResult classes

---
+++ packages/cqrs_wrapper/lib/src/cqrs_result.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:cqrs_wrapper/src/cqrs_error.dart';
+import 'package:equatable/equatable.dart';
+
+sealed class CqrsResult<T, E extends CqrsError> extends Equatable {
+  const CqrsResult();
+
+  CqrsSuccess<T, E>? get asSuccess => switch (this) {
+        final CqrsSuccess<T, E> s => s,
+        CqrsFailure() => null,
+      };
+
+  CqrsFailure<T, E>? get asFailure => switch (this) {
+        CqrsSuccess() => null,
+        final CqrsFailure<T, E> f => f,
+      };
+
+  bool get isSuccess => asSuccess != null;
+  bool get isFailure => asFailure != null;
+
+  T? get data => asSuccess?.data;
+  E? get error => asFailure?.error;
+}
+
+final class CqrsSuccess<T, E extends CqrsError> extends CqrsResult<T, E> {
+  const CqrsSuccess(this.data);
+
+  @override
+  final T data;
+
+  @override
+  List<Object?> get props => [data];
+}
+
+final class CqrsFailure<T, E extends CqrsError> extends CqrsResult<T, E> {
+  const CqrsFailure(this.error);
+
+  @override
+  final E error;
+
+  @override
+  List<Object?> get props => [error];
+}
