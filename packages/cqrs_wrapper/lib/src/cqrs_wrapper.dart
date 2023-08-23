3962b4a3476eae800d11c307e89f19298fbbf462
Jakub Tarkowski <jtarkowski1227@gmail.com>
2023-08-23 22:24:01 +0200

    Implement missing cases for error handling in CqrsWrapper

--- packages/cqrs_wrapper/lib/src/cqrs_wrapper.dart
+++ packages/cqrs_wrapper/lib/src/cqrs_wrapper.dart
@@ -10,18 +10,46 @@ class CqrsWrapper {
   const CqrsWrapper({
     required Cqrs cqrs,
     required Logger logger,
+    void Function(CqrsQueryError error)? onQueryError,
+    void Function(CqrsCommandError error)? onCommandError,
   })  : _cqrs = cqrs,
-        _logger = logger;
+        _logger = logger,
+        _onQueryError = onQueryError,
+        _onCommandError = onCommandError;
 
   final Cqrs _cqrs;
   final Logger _logger;
 
-  // void onQueryError(CqrsQueryError error) {}
+  final void Function(CqrsQueryError error)? _onQueryError;
+  final void Function(CqrsCommandError error)? _onCommandError;
 
   Future<CqrsQueryResult<T>> noThrowGet<T>(Query<T> query) async {
+    final result = await _noThrowGet(query);
+    final error = result.error;
+
+    if (result.isFailure && error != null) {
+      _onQueryError?.call(error);
+    }
+
+    return result;
+  }
+
+  Future<CqrsCommandResult> noThrowRun(Command command) async {
+    final result = await _noThrowRun(command);
+    final error = result.error;
+
+    if (result.isFailure && error != null) {
+      _onCommandError?.call(error);
+    }
+
+    return result;
+  }
+
+  Future<CqrsQueryResult<T>> _noThrowGet<T>(Query<T> query) async {
     try {
       final data = await _cqrs.get(query);
       _logger.info('Query ${query.runtimeType} executed successfully.');
+
       return CqrsSuccess(data);
     } on SocketException catch (e, s) {
       _logger.severe(
@@ -29,6 +57,7 @@ class CqrsWrapper {
         e,
         s,
       );
+
       return const CqrsFailure(CqrsQueryError.network);
     } catch (e, s) {
       _logger.severe('Query ${query.runtimeType} failed unexpectedly.', e, s);
@@ -36,6 +65,7 @@ class CqrsWrapper {
       if (e is! CqrsException) {
         return const CqrsFailure(CqrsQueryError.unknown);
       }
+
       return switch (e.response.statusCode) {
         401 => const CqrsFailure(CqrsQueryError.authentication),
         403 => const CqrsFailure(CqrsQueryError.forbiddenAccess),
@@ -44,7 +74,7 @@ class CqrsWrapper {
     }
   }
 
-  Future<CqrsCommandResult> noThrowRun(Command command) async {
+  Future<CqrsCommandResult> _noThrowRun(Command command) async {
     try {
       final result = await _cqrs.run(command);
 
@@ -63,18 +93,12 @@ class CqrsWrapper {
           ' ValidationErrors: [$buffer]',
         );
 
-        if (result.hasError(401)) {
-          return CqrsCommandResult.nonValidationError(
-            CqrsCommandError.authentication,
-          );
-        } else if (result.hasError(403)) {
-          return CqrsCommandResult.nonValidationError(
-            CqrsCommandError.forbiddenAccess,
-          );
-        } else if (result.hasError(422)) {
+        if (result.hasError(422)) {
           return CqrsCommandResult.validationError(result.errors);
         } else {
-          return CqrsCommandResult.nonValidationError(CqrsCommandError.unknown);
+          return CqrsCommandResult.nonValidationError(
+            CqrsCommandError.unknown,
+          );
         }
       }
     } on SocketException catch (e, s) {
@@ -83,6 +107,7 @@ class CqrsWrapper {
         e,
         s,
       );
+
       return CqrsCommandResult.nonValidationError(
         CqrsCommandError.forbiddenAccess,
       );
@@ -92,7 +117,24 @@ class CqrsWrapper {
         e,
         s,
       );
-      return CqrsCommandResult.nonValidationError(CqrsCommandError.unknown);
+
+      if (e is! CqrsException) {
+        return CqrsCommandResult.nonValidationError(
+          CqrsCommandError.unknown,
+        );
+      }
+
+      return switch (e.response.statusCode) {
+        401 => CqrsCommandResult.nonValidationError(
+            CqrsCommandError.authentication,
+          ),
+        403 => CqrsCommandResult.nonValidationError(
+            CqrsCommandError.forbiddenAccess,
+          ),
+        _ => CqrsCommandResult.nonValidationError(
+            CqrsCommandError.unknown,
+          ),
+      };
     }
   }
 }
