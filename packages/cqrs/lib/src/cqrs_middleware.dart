9e7b5ed556b79b9da20fc7226dd11b282ef17ba9
Jakub Tarkowski <jtarkowski1227@gmail.com>
2023-09-15 23:07:08 +0200

    Add default middleware method bodies

--- packages/cqrs/lib/src/cqrs_middleware.dart
+++ packages/cqrs/lib/src/cqrs_middleware.dart
@@ -1,5 +1,19 @@
-import 'package:cqrs/src/cqrs_error.dart';
-import 'package:cqrs/src/cqrs_result.dart';
+// Copyright 2023 LeanCode Sp. z o.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'cqrs_error.dart';
+import 'cqrs_result.dart';
 
 /// Abstract class for CQRS result handlers. Can be used to specify certain
 /// behaviors that will be triggered for given errors i.e. showing snackbar
@@ -9,14 +23,16 @@ abstract class CqrsMiddleware {
   const CqrsMiddleware();
 
   /// Handle and return query result. If no modification of query result
-  /// is needed then return the same `result` that is passed to the method.
+  /// is needed then return the same `result` that was passed to the method.
   Future<CqrsQueryResult<T, CqrsError>> handleQueryResult<T>(
     CqrsQueryResult<T, CqrsError> result,
-  );
+  ) =>
+      Future.value(result);
 
   /// Handle and return command result. If no modification of command result
-  /// is needed then return the same `result` that is passed to the method.
+  /// is needed then return the same `result` that was passed to the method.
   Future<CqrsCommandResult<CqrsError>> handleCommandResult(
     CqrsCommandResult<CqrsError> result,
-  );
+  ) =>
+      Future.value(result);
 }
