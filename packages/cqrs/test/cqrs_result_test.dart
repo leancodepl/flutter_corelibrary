6e73852a4d79da3b8a5b27e24a358cc50f5bb0fd
Jakub Tarkowski <jtarkowski1227@gmail.com>
2023-09-18 18:15:19 +0200

    Add CqrsCommandResult tests

--- packages/cqrs/test/cqrs_result_test.dart
+++ packages/cqrs/test/cqrs_result_test.dart
@@ -28,6 +28,55 @@ void main() {
     });
   });
 
+  group('CqrsCommandResult', () {
+    group('fields values are correct', () {
+      test('when constructed as success', () {
+        const result = CqrsCommandSuccess<CqrsError>();
+
+        expect(result.asSuccess, result);
+        expect(result.asFailure, null);
+        expect(result.isSuccess, true);
+        expect(result.isFailure, false);
+        expect(result.isInvalid, false);
+        expect(result.validationErrors, const <ValidationError>[]);
+        expect(result.error, null);
+      });
+
+      test('when constructed as failure without validation errors', () {
+        const result = CqrsCommandFailure<CqrsError>(CqrsError.unknown);
+
+        expect(result.asSuccess, null);
+        expect(result.asFailure, result);
+        expect(result.isSuccess, false);
+        expect(result.isFailure, true);
+        expect(result.isInvalid, false);
+        expect(result.validationErrors, const <ValidationError>[]);
+        expect(result.error, CqrsError.unknown);
+      });
+
+      test('when constructed as failure with validation errors', () {
+        const result = CqrsCommandFailure<CqrsError>(
+          CqrsError.validation,
+          validationErrors: [
+            ValidationError(123, 'Test message', 'SomeProperty'),
+            ValidationError(456, 'Another message', 'OtherProperty')
+          ],
+        );
+
+        expect(result.asSuccess, null);
+        expect(result.asFailure, result);
+        expect(result.isSuccess, false);
+        expect(result.isFailure, true);
+        expect(result.isInvalid, true);
+        expect(result.validationErrors, const <ValidationError>[
+          ValidationError(123, 'Test message', 'SomeProperty'),
+          ValidationError(456, 'Another message', 'OtherProperty')
+        ]);
+        expect(result.error, CqrsError.validation);
+      });
+    });
+  });
+
   group('CqrsOperationResult', () {
     group('fields values are correct', () {
       test('when constructed as success', () {
