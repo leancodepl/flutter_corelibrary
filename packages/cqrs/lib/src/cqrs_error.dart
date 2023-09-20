1a32a1a7bad0c604765ddd48c00fbd8ba9d820ef
Jakub Tarkowski <jtarkowski1227@gmail.com>
2023-09-20 14:02:41 +0200

    Update CqrsError docs

--- packages/cqrs/lib/src/cqrs_error.dart
+++ packages/cqrs/lib/src/cqrs_error.dart
@@ -14,18 +14,18 @@
 
 /// Error types that are recognized by Cqrs.
 enum CqrsError {
-  /// Represents a network/socket error
+  /// Represents a network/socket error.
   network,
 
-  /// Represents a HTTP 401 authetication error
+  /// Represents a HTTP 401 authentication error.
   authentication,
 
-  /// Represents a HTTP 403 forbidden access error
+  /// Represents a HTTP 403 forbidden access error.
   forbiddenAccess,
 
-  /// Represents a HTTP 422 validation error
+  /// Represents a HTTP 422 validation error.
   validation,
 
-  /// Represents a generic error which covers all remaining errors
+  /// Represents a generic error which covers all remaining errors.
   unknown,
 }
