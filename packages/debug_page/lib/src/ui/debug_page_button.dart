61cd7912294637a004173c42198c72ae55275b48
Jakub Lange <jakub.lange.gd@gmail.com>
2024-05-26 13:39:08 +0200

    Push state to a leaf, do not use single child row, bump lints

--- packages/debug_page/lib/src/ui/debug_page_button.dart
+++ packages/debug_page/lib/src/ui/debug_page_button.dart
@@ -1,20 +1,13 @@
 import 'package:debug_page/debug_page.dart';
 import 'package:flutter/material.dart';
 
-class DebugPageButton extends StatefulWidget {
+class DebugPageButton extends StatelessWidget {
   const DebugPageButton({
     super.key,
     required DebugPageController controller,
   }) : _controller = controller;
 
   final DebugPageController _controller;
-
-  @override
-  State<StatefulWidget> createState() => DebugPageButtonState();
-}
-
-class DebugPageButtonState extends State<DebugPageButton> {
-  Alignment _alignment = Alignment.bottomRight;
 
   @override
   Widget build(BuildContext context) {
@@ -24,42 +17,10 @@ class DebugPageButtonState extends State<DebugPageButton> {
         initialEntries: [
           OverlayEntry(
             builder: (context) {
-              final Size(:height, :width) = MediaQuery.sizeOf(context);
-              final horizontalCenter = width / 2;
-              final verticalCenter = height / 2;
-
               return SafeArea(
                 child: Padding(
                   padding: const EdgeInsets.all(16),
-                  child: Align(
-                    alignment: _alignment,
-                    child: Draggable(
-                      feedback: Align(
-                        alignment: _alignment,
-                        child: _Button(widget._controller),
-                      ),
-                      onDragEnd: (details) {
-                        final Offset(:dx, :dy) = details.offset;
-                        final top = dy < verticalCenter;
-                        final right = dx > horizontalCenter;
-
-                        final newAlignment = switch ((top, right)) {
-                          (true, true) => Alignment.topRight,
-                          (true, false) => Alignment.topLeft,
-                          (false, true) => Alignment.bottomRight,
-                          (false, false) => Alignment.bottomLeft,
-                        };
-
-                        if (newAlignment != _alignment) {
-                          setState(() {
-                            _alignment = newAlignment;
-                          });
-                        }
-                      },
-                      childWhenDragging: const SizedBox(),
-                      child: _Button(widget._controller),
-                    ),
-                  ),
+                  child: _DraggableFloatingButton(controller: _controller),
                 ),
               );
             },
@@ -70,16 +31,58 @@ class DebugPageButtonState extends State<DebugPageButton> {
   }
 }
 
-class _Button extends StatelessWidget {
-  const _Button(this._controller);
+class _DraggableFloatingButton extends StatefulWidget {
+  const _DraggableFloatingButton({
+    required DebugPageController controller,
+  }) : _controller = controller;
 
   final DebugPageController _controller;
 
   @override
+  State<StatefulWidget> createState() => _DraggableFloatingButtonState();
+}
+
+class _DraggableFloatingButtonState extends State<_DraggableFloatingButton> {
+  _DraggableFloatingButtonState();
+
+  Alignment _alignment = Alignment.bottomRight;
+
+  @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () => _controller.visible.value = true,
+    final Size(:height, :width) = MediaQuery.sizeOf(context);
+    final horizontalCenter = width / 2;
+    final verticalCenter = height / 2;
+
+    final floatingActionButton = FloatingActionButton(
+      onPressed: () => widget._controller.visible.value = true,
       child: const Icon(Icons.bug_report),
+    );
+
+    return Align(
+      alignment: _alignment,
+      child: Draggable(
+        feedback: floatingActionButton,
+        onDragEnd: (details) {
+          final Offset(:dx, :dy) = details.offset;
+          final top = dy < verticalCenter;
+          final right = dx > horizontalCenter;
+
+          final newAlignment = switch ((top, right)) {
+            (true, true) => Alignment.topRight,
+            (true, false) => Alignment.topLeft,
+            (false, true) => Alignment.bottomRight,
+            (false, false) => Alignment.bottomLeft,
+          };
+
+          if (newAlignment != _alignment) {
+            setState(() {
+              _alignment = newAlignment;
+            });
+          }
+        },
+        childWhenDragging: const SizedBox(),
+        child: floatingActionButton,
+      ),
     );
   }
 }
