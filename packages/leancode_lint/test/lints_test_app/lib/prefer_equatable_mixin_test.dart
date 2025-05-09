cd0e9224e24e3f0333f7f7828092be0df8b8564d
PiotrRogulski <piotr.rogulski@leancode.pl>
2025-05-09 14:54:47 +0200

    Add tests for `prefer_equatable_mixin`

---
+++ packages/leancode_lint/test/lints_test_app/lib/prefer_equatable_mixin_test.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+// Directly extending Equatable is flagged
+class MyState
+    extends
+        // expect_lint: prefer_equatable_mixin
+        Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+// Transitively extending Equatable is not flagged
+class MyState2 extends MyState {
+  @override
+  List<Object?> get props => [];
+}
+
+// Using EquatableMixin is not flagged
+class MyState3 with EquatableMixin {
+  @override
+  List<Object?> get props => [];
+}
