777d8a2e90d98c30f0951fe44dbc1754f86281e9
Artur Wiśniewski <artur.wisniewski.dev@gmail.com>
2025-07-31 16:11:15 +0200

    feat: add avoid_missing_dispose lint

---
+++ packages/leancode_lint/lib/lints/avoid_missing_dispose.dart
@@ -0,0 +1,253 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/element2.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer/error/error.dart' hide LintCode;
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:leancode_lint/utils.dart';
+
+/// Checks for proper disposal of resources in StatefulWidget classes.
+/// Warns when disposable resources are not properly disposed in the dispose() method.
+class AvoidMissingDispose extends DartLintRule {
+  AvoidMissingDispose() : super(code: _createCode());
+
+  static LintCode _createCode() => const LintCode(
+    name: ruleName,
+    problemMessage: 'Resource should be disposed in the dispose() method.',
+    correctionMessage: 'Add disposal of this resource in the dispose() method.',
+    errorSeverity: ErrorSeverity.WARNING,
+  );
+
+  static const ruleName = 'avoid_missing_dispose';
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addFieldDeclaration((node) {
+      final classNode = _getContainingClass(node);
+      if (classNode == null) {
+        return;
+      }
+
+      if (_isWidget(classNode) && !_isFieldUsedByConstructor(classNode, node)) {
+        reporter.atNode(node, _createCode());
+        return;
+      }
+
+      if (!_isStateOfWidget(classNode)) {
+        return;
+      }
+
+      final type = _getFieldDeclarationType(node);
+
+      if (type == null || !_isDisposable(type)) {
+        return;
+      }
+
+      final disposeExpressions = _DisposeExpressionsGatherer.gatherForTarget(
+        node: classNode,
+        targetName: node.fields.variables.first.name.lexeme,
+      );
+
+      if (disposeExpressions.isEmpty) {
+        reporter.atNode(node, _createCode());
+      }
+    });
+    context.registry.addInstanceCreationExpression((node) {
+      final classNode = _getContainingClass(node);
+
+      if (classNode == null ||
+          !(_isWidget(classNode) || _isStateOfWidget(classNode))) {
+        return;
+      }
+
+      final type = switch (node.staticType) {
+        final InterfaceType type => type,
+        _ => null,
+      };
+
+      if (type == null || !_isDisposable(type)) {
+        return;
+      }
+
+      if (_isInReturnWidget(node)) {
+        reporter.atNode(node, _createCode());
+        return;
+      }
+    });
+  }
+
+  InterfaceType? _getFieldDeclarationType(FieldDeclaration field) {
+    if (field.fields.type?.type case final InterfaceType type) {
+      return type;
+    }
+    if (field.fields.variables.first.initializer?.staticType
+        case final InterfaceType? type) {
+      return type;
+    }
+    return null;
+  }
+
+  bool _isFieldUsedByConstructor(
+    ClassDeclaration classNode,
+    FieldDeclaration node,
+  ) {
+    final constructorDeclaration = _getConstructorDeclaration(classNode);
+    if (constructorDeclaration == null) {
+      return false;
+    }
+
+    return _hasConstructorParameterOrInitializer(
+      constructorDeclaration,
+      node.fields.variables.first.name.lexeme,
+    );
+  }
+
+  bool _hasConstructorParameterOrInitializer(
+    ConstructorDeclaration constructorDeclaration,
+    String parameterName,
+  ) {
+    for (final parameter in constructorDeclaration.parameters.parameters) {
+      if (parameter.name?.lexeme == parameterName) {
+        return true;
+      }
+    }
+    for (final initializer in constructorDeclaration.initializers) {
+      if (initializer case final ConstructorFieldInitializer initializer) {
+        if (initializer.fieldName.name == parameterName) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  ConstructorDeclaration? _getConstructorDeclaration(
+    ClassDeclaration classNode,
+  ) {
+    for (final member in classNode.members) {
+      if (member case final ConstructorDeclaration constructorDeclaration) {
+        return constructorDeclaration;
+      }
+    }
+    return null;
+  }
+
+  ClassDeclaration? _getContainingClass(AstNode node) {
+    var classNode = node.parent;
+    while (classNode != null && classNode is! ClassDeclaration) {
+      classNode = classNode.parent;
+    }
+    if (classNode case final ClassDeclaration classNode) {
+      return classNode;
+    }
+    return null;
+  }
+
+  bool _isDisposable(InterfaceType type) {
+    // Checks if current class has dispose method
+    var hasDispose = type.methods2.any((method) => method.name3 == 'dispose');
+    // Checks if current class has dispose method in the inherited members
+    if (!hasDispose) {
+      hasDispose =
+          type.element3.inheritedMembers.entries.firstWhereOrNull(
+            (entry) =>
+                entry.key.name == 'dispose' &&
+                entry.value.baseElement is MethodElement2,
+          ) !=
+          null;
+    }
+    return hasDispose;
+  }
+
+  bool _isType(InterfaceType type, String typeName) {
+    if (type.element3.name3 == typeName) {
+      return true;
+    }
+    return type.element3.allSupertypes.any(
+      (supertype) => supertype.element3.name3 == typeName,
+    );
+  }
+
+  bool _isInReturnWidget(InstanceCreationExpression node) {
+    AstNode? currentNode = node;
+    var returnsWidget = false;
+    var isInReturn = false;
+    while (currentNode != null && !(returnsWidget && isInReturn)) {
+      if (!isInReturn &&
+          (currentNode is ReturnStatement ||
+              currentNode is ExpressionFunctionBody)) {
+        isInReturn = true;
+      }
+      if (currentNode is MethodDeclaration) {
+        if (currentNode.returnType case final NamedType returnType
+            when returnType.type is InterfaceType &&
+                _isType(returnType.type! as InterfaceType, 'Widget')) {
+          returnsWidget = true;
+        } else {
+          return false;
+        }
+      }
+      currentNode = currentNode.parent;
+    }
+    return returnsWidget && isInReturn;
+  }
+
+  bool _isStateOfWidget(ClassDeclaration classNode) {
+    // Suggested ClassElement2 doesn't work in this case
+    // ignore: deprecated_member_use
+    if (classNode.declaredFragment case final ClassElement element) {
+      return element.allSupertypes.any(
+        (type) => type.element3.name3 == 'State',
+      );
+    }
+    return false;
+  }
+
+  bool _isWidget(ClassDeclaration classNode) {
+    // Suggested ClassElement2 doesn't work in this case
+    // ignore: deprecated_member_use
+    if (classNode.declaredFragment case final ClassElement element) {
+      return element.allSupertypes.any(
+        (type) => type.element3.name3 == 'Widget',
+      );
+    }
+    return false;
+  }
+}
+
+class _DisposeExpressionsGatherer extends GeneralizingAstVisitor<void> {
+  _DisposeExpressionsGatherer({required this.targetName});
+  final String targetName;
+
+  final List<InvocationExpression> _disposeExpressions = [];
+
+  static List<InvocationExpression> gatherForTarget({
+    required AstNode node,
+    required String targetName,
+  }) {
+    final visitor = _DisposeExpressionsGatherer(targetName: targetName);
+    node.accept(visitor);
+    return visitor._disposeExpressions;
+  }
+
+  static const disposeMethodName = 'dispose';
+
+  @override
+  void visitExpressionStatement(ExpressionStatement node) {
+    if (node.expression case final MethodInvocation methodInvocation) {
+      if (methodInvocation.methodName.name == disposeMethodName) {
+        if (methodInvocation.target case final SimpleIdentifier target) {
+          if (target.name == targetName) {
+            _disposeExpressions.add(methodInvocation);
+          }
+        }
+      }
+    }
+  }
+}
