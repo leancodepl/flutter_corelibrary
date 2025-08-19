2f9eabc4e6e353778ac94c22e07d194757f1bb95
Artur Wiśniewski <artur.wisniewski.dev@gmail.com>
2025-08-19 15:42:28 +0200

    feat: prefer center over align lint (#472)
    * feat: add prefer center over align lint
    
    * feat: add prefer center over align lint readme
    
    * refactor: removed ruleName variable
    
    * refactor: prefer_center_over_align lint
    
    * refactor: cr requests
    
    * feat: add todo with hints for feature refactor

---
+++ packages/leancode_lint/lib/lints/prefer_center_over_align.dart
@@ -0,0 +1,123 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/error.dart' as error;
+import 'package:analyzer/error/listener.dart';
+import 'package:analyzer_plugin/utilities/range_factory.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:leancode_lint/helpers.dart';
+
+class PreferCenterOverAlign extends DartLintRule {
+  const PreferCenterOverAlign()
+    : super(
+        code: const LintCode(
+          name: 'prefer_center_over_align',
+          problemMessage:
+              'Use the Center widget instead of the Align widget with the alignment parameter set to Alignment.center',
+        ),
+      );
+
+  @override
+  List<Fix> getFixes() => [_PreferCenterOverAlignFix()];
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addInstanceCreationExpression((node) {
+      final data = _analyzeAlignInstanceCreationExpression(node);
+      if (data case final data? when data.isAlignmentCenter) {
+        reporter.atNode(node.constructorName, code, data: data);
+      }
+    });
+  }
+
+  _PreferCenterOverAlignData? _analyzeAlignInstanceCreationExpression(
+    InstanceCreationExpression node,
+  ) {
+    if (!isExpressionExactlyType(node, 'Align', 'flutter')) {
+      return null;
+    }
+    final arguments = node.argumentList.arguments;
+    var hasAlignmentArgument = false;
+
+    for (final argument in arguments.whereType<NamedExpression>()) {
+      if (argument.name.label.name == 'alignment') {
+        hasAlignmentArgument = true;
+        if (_isValueAlignmentCenter(argument)) {
+          return _PreferCenterOverAlignData(
+            isAlignmentCenter: true,
+            alignmentExpression: argument,
+            listOfArguments: arguments,
+          );
+        }
+      }
+    }
+    if (!hasAlignmentArgument) {
+      return _PreferCenterOverAlignData(isAlignmentCenter: true);
+    }
+    return null;
+  }
+
+  // TODO: Refactor method below with: computeConstantValue() (https://github.com/dart-lang/sdk/blob/4c4a1d3815a754f7a14112fb0f96030869e305f9/pkg/analyzer/lib/src/dart/ast/ast.dart#L7663)
+  // once leancode_lint is upgraded
+  bool _isValueAlignmentCenter(NamedExpression argument) {
+    return switch (argument.expression) {
+      PrefixedIdentifier(name: 'Alignment.center') => true,
+      InstanceCreationExpression(
+        staticType: final type,
+        argumentList: ArgumentList(:final arguments),
+      )
+          when type?.getDisplayString() == 'Alignment' &&
+              arguments.length == 2 =>
+        _isEveryValueZero(arguments),
+      _ => false,
+    };
+  }
+
+  bool _isEveryValueZero(List<Expression> arguments) => arguments.every(
+    (argument) => switch (argument) {
+      IntegerLiteral(value: 0) || DoubleLiteral(value: 0) => true,
+      _ => false,
+    },
+  );
+}
+
+class _PreferCenterOverAlignData {
+  _PreferCenterOverAlignData({
+    required this.isAlignmentCenter,
+    this.alignmentExpression,
+    this.listOfArguments,
+  });
+
+  final bool isAlignmentCenter;
+  final Expression? alignmentExpression;
+  final NodeList<Expression>? listOfArguments;
+}
+
+class _PreferCenterOverAlignFix extends DartFix {
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ChangeReporter reporter,
+    CustomLintContext context,
+    error.AnalysisError analysisError,
+    List<error.AnalysisError> errors,
+  ) {
+    if (analysisError.data case final _PreferCenterOverAlignData data) {
+      reporter
+          .createChangeBuilder(message: 'Replace with Center', priority: 1)
+          .addDartFileEdit((builder) {
+            builder.addSimpleReplacement(analysisError.sourceRange, 'Center');
+            if (data case _PreferCenterOverAlignData(
+              :final listOfArguments?,
+              :final alignmentExpression?,
+            )) {
+              builder.addDeletion(
+                range.nodeInList(listOfArguments, alignmentExpression),
+              );
+            }
+          });
+    }
+  }
+}
