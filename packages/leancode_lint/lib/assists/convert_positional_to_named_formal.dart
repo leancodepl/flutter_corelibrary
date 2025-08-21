47c7a1847306a9522b0075e46abea66ef237ea67
Piotr Rogulski <piter.rogulski@gmail.com>
2025-08-21 15:13:12 +0200

    Restore assists

--- packages/leancode_lint/lib/assists/convert_positional_to_named_formal.dart
+++ packages/leancode_lint/lib/assists/convert_positional_to_named_formal.dart
@@ -1,102 +1,109 @@
-// import 'package:analyzer/dart/ast/syntactic_entity.dart';
-// import 'package:analyzer/dart/ast/token.dart';
-// import 'package:analyzer/source/source_range.dart';
-// import 'package:custom_lint_builder/custom_lint_builder.dart';
-// import 'package:leancode_lint/utils.dart';
-//
-// /// Converts required positional parameters to named ones.
-// ///
-// /// **Example**:
-// ///
-// /// ```dart
-// /// class MyType<T extends Object> {
-// ///   const MyType(
-// ///     this.pos0,
-// ///     this.hello, {
-// ///     this.named1,
-// ///   });
-// ///
-// ///   final String pos0;
-// ///   final OtherType hello;
-// ///   final int? named1;
-// /// }
-// /// ```
-// ///
-// /// When applied to `pos0`, the result is
-// ///
-// /// ```dart
-// /// class MyType<T extends Object> {
-// ///   const MyType(
-// ///     this.hello, {
-// ///     required this.pos0,
-// ///     this.named1,
-// ///   });
-// ///
-// ///   final String pos0;
-// ///   final OtherType hello;
-// ///   final int? named1;
-// /// }
-// /// ```
-// class ConvertPositionalToNamedFormal extends DartAssist {
-//   @override
-//   void run(
-//     CustomLintResolver resolver,
-//     ChangeReporter reporter,
-//     CustomLintContext context,
-//     SourceRange target,
-//   ) {
-//     context.registry.addFormalParameterList((node) {
-//       // parameter list has positional optional parameters, named params cannot be added
-//       if (node.leftDelimiter?.kind == TokenType.OPEN_SQUARE_BRACKET.kind) {
-//         return;
-//       }
-//
-//       final parameters = node.parameters.where(
-//         (parameter) => parameter.isRequiredPositional,
-//       );
-//
-//       final nextItem = parameters.skip(1).cast<SyntacticEntity>().followedBy([
-//         node.leftDelimiter ?? node.rightParenthesis,
-//       ]);
-//
-//       final found = parameters
-//           .zip(nextItem)
-//           .map(
-//             (e) => (e.$1, SourceRange(e.$1.offset, e.$2.offset - e.$1.offset)),
-//           )
-//           .firstWhereOrNull((e) => target.intersects(e.$2));
-//
-//       if (found == null) {
-//         return;
-//       }
-//
-//       final (parameter, sourceRange) = found;
-//
-//       reporter
-//           .createChangeBuilder(
-//             message: 'Convert to named formal parameter',
-//             priority: 1,
-//           )
-//           .addDartFileEdit((builder) {
-//             // delete positional parameter
-//             builder.addDeletion(sourceRange);
-//
-//             final namedParam =
-//                 '${Keyword.REQUIRED.lexeme} ${Keyword.THIS.lexeme}.${parameter.name},';
-//
-//             if (node.leftDelimiter case final brace?) {
-//               // has named parameters, add to list
-//               builder.addSimpleInsertion(brace.offset + 1, namedParam);
-//             } else {
-//               // no named parameters, create
-//               builder.addSimpleInsertion(
-//                 node.rightParenthesis.offset,
-//                 '{$namedParam}',
-//               );
-//             }
-//
-//             builder.format(node.sourceRange);
-//           });
-//     });
-//   }
-// }
+import 'package:analysis_server_plugin/edit/dart/correction_producer.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/syntactic_entity.dart';
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:analyzer/source/source_range.dart';
+import 'package:analyzer_plugin/utilities/assist/assist.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
+import 'package:leancode_lint/utils.dart';
+
+/// Converts required positional parameters to named ones.
+///
+/// **Example**:
+///
+/// ```dart
+/// class MyType<T extends Object> {
+///   const MyType(
+///     this.pos0,
+///     this.hello, {
+///     this.named1,
+///   });
+///
+///   final String pos0;
+///   final OtherType hello;
+///   final int? named1;
+/// }
+/// ```
+///
+/// When applied to `pos0`, the result is
+///
+/// ```dart
+/// class MyType<T extends Object> {
+///   const MyType(
+///     this.hello, {
+///     required this.pos0,
+///     this.named1,
+///   });
+///
+///   final String pos0;
+///   final OtherType hello;
+///   final int? named1;
+/// }
+/// ```
+class ConvertPositionalToNamedFormal extends ResolvedCorrectionProducer {
+  ConvertPositionalToNamedFormal({required super.context});
+
+  @override
+  AssistKind? get assistKind => const AssistKind(
+    'leancode.lint.convertPositionalToNamedFormal',
+    50,
+    'Convert to a named formal parameter',
+  );
+
+  @override
+  CorrectionApplicability get applicability =>
+      CorrectionApplicability.singleLocation;
+
+  @override
+  Future<void> compute(ChangeBuilder builder) async {
+    if (node case final FormalParameterList node) {
+      // parameter list has positional optional parameters, named params cannot be added
+      if (node.leftDelimiter?.kind == TokenType.OPEN_SQUARE_BRACKET.kind) {
+        return;
+      }
+
+      final parameters = node.parameters.where(
+        (parameter) => parameter.isRequiredPositional,
+      );
+
+      final nextItem = parameters.skip(1).cast<SyntacticEntity>().followedBy([
+        node.leftDelimiter ?? node.rightParenthesis,
+      ]);
+
+      final target = SourceRange(selectionOffset, selectionLength);
+      final found = parameters
+          .zip(nextItem)
+          .map(
+            (e) => (e.$1, SourceRange(e.$1.offset, e.$2.offset - e.$1.offset)),
+          )
+          .firstWhereOrNull((e) => target.intersects(e.$2));
+
+      if (found == null) {
+        return;
+      }
+
+      final (parameter, sourceRange) = found;
+
+      await builder.addDartFileEdit(file, (builder) {
+        // delete positional parameter
+        builder.addDeletion(sourceRange);
+
+        final namedParam =
+            '${Keyword.REQUIRED.lexeme} ${Keyword.THIS.lexeme}.${parameter.name},';
+
+        if (node.leftDelimiter case final brace?) {
+          // has named parameters, add to list
+          builder.addSimpleInsertion(brace.offset + 1, namedParam);
+        } else {
+          // no named parameters, create
+          builder.addSimpleInsertion(
+            node.rightParenthesis.offset,
+            '{$namedParam}',
+          );
+        }
+
+        builder.format(SourceRange(node.offset, node.length));
+      });
+    }
+  }
+}
--- packages/leancode_lint/lib/main.dart
+++ packages/leancode_lint/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:analysis_server_plugin/plugin.dart';
 import 'package:analysis_server_plugin/registry.dart';
+import 'package:leancode_lint/assists/convert_iterable_map_to_collection_for.dart';
+import 'package:leancode_lint/assists/convert_positional_to_named_formal.dart';
+import 'package:leancode_lint/assists/convert_record_into_nominal_type.dart';
 import 'package:leancode_lint/lints/add_cubit_suffix_for_cubits.dart';
 import 'package:leancode_lint/lints/avoid_conditional_hooks.dart';
 import 'package:leancode_lint/lints/avoid_single_child_in_multi_child_widget.dart';
@@ -24,6 +27,9 @@ class _Linter extends Plugin {
   @override
   void register(PluginRegistry registry) {
     registry
+      ..registerAssist(ConvertRecordIntoNominalType.new)
+      ..registerAssist(ConvertPositionalToNamedFormal.new)
+      ..registerAssist(ConvertIterableMapToCollectionFor.new)
       ..registerWarningRule(StartCommentsWithSpace()..registerFixes(registry))
       ..registerWarningRule(AddCubitSuffixForYourCubits())
       ..registerWarningRule(CatchParameterNames())
