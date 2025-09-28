a7e00b74c1f1465cef8b32de49a2f08ad19341d5
Piotr Rogulski <piter.rogulski@gmail.com>
2025-09-28 23:29:55 +0200

    Bring back the `constructor_parameters_and_fields_should_have_the_same_order` lint

--- packages/leancode_lint/lib/lints/constructor_parameters_and_fields_should_have_the_same_order.dart
+++ packages/leancode_lint/lib/lints/constructor_parameters_and_fields_should_have_the_same_order.dart
@@ -1,130 +1,136 @@
-// import 'package:analyzer/dart/ast/ast.dart';
-// import 'package:analyzer/error/error.dart' hide LintCode;
-// import 'package:analyzer/error/listener.dart';
-// import 'package:custom_lint_builder/custom_lint_builder.dart';
-//
-// /// Displays warning when constructor's parameters' order vary from class
-// /// declared fields order. Works for the both named and unnamed parameters.
-// class ConstructorParametersAndFieldsShouldHaveTheSameOrder
-//     extends DartLintRule {
-//   const ConstructorParametersAndFieldsShouldHaveTheSameOrder()
-//     : super(
-//         code: const LintCode(
-//           name: 'constructor_parameters_and_fields_should_have_the_same_order',
-//           problemMessage:
-//               'Class parameters and fields should have the same order.',
-//           errorSeverity: DiagnosticSeverity.WARNING,
-//         ),
-//       );
-//
-//   // TODO: disabled until stabilized. Add documentation.
-//   @override
-//   bool get enabledByDefault => false;
-//
-//   @override
-//   void run(
-//     CustomLintResolver resolver,
-//     DiagnosticReporter reporter,
-//     CustomLintContext context,
-//   ) {
-//     context.registry.addClassDeclaration((node) {
-//       final fields = node.members.whereType<FieldDeclaration>().toList();
-//
-//       if (fields.isEmpty) {
-//         return;
-//       }
-//
-//       final constructors = node.members.whereType<ConstructorDeclaration>();
-//       for (final constructor in constructors) {
-//         if (!_hasValidOrder(constructor, fields)) {
-//           reporter.atNode(constructor, code);
-//         }
-//       }
-//     });
-//   }
-//
-//   bool _hasValidOrder(
-//     ConstructorDeclaration constructor,
-//     List<FieldDeclaration> fields,
-//   ) {
-//     final parameters = constructor.parameters.parameters;
-//     if (parameters.isEmpty) {
-//       return true;
-//     }
-//
-//     final namedParameters = parameters
-//         .where((parameter) => parameter.isNamed && _isNotSuperFormal(parameter))
-//         .toList();
-//
-//     final unnamedParameters = parameters
-//         .where(
-//           (parameter) => !parameter.isNamed && _isNotSuperFormal(parameter),
-//         )
-//         .toList();
-//
-//     final fieldsWithNamedParameters = fields
-//         .where(
-//           (field) => namedParameters.any(
-//             (parameter) => _compareEffectiveNames(field, parameter),
-//           ),
-//         )
-//         .toList();
-//
-//     final fieldsWithUnnamedParameters = fields
-//         .where(
-//           (field) => unnamedParameters.any(
-//             (parameter) => _compareEffectiveNames(field, parameter),
-//           ),
-//         )
-//         .toList();
-//
-//     for (
-//       var i = 0;
-//       i < namedParameters.length && i < fieldsWithNamedParameters.length;
-//       i++
-//     ) {
-//       if (!_compareEffectiveNames(
-//         fieldsWithNamedParameters[i],
-//         namedParameters[i],
-//       )) {
-//         return false;
-//       }
-//     }
-//
-//     for (
-//       var i = 0;
-//       i < unnamedParameters.length && i < fieldsWithUnnamedParameters.length;
-//       i++
-//     ) {
-//       if (!_compareEffectiveNames(
-//         fieldsWithUnnamedParameters[i],
-//         unnamedParameters[i],
-//       )) {
-//         return false;
-//       }
-//     }
-//
-//     return true;
-//   }
-//
-//   bool _isNotSuperFormal(FormalParameter parameter) =>
-//       !(parameter.declaredFragment?.element.isSuperFormal ?? false);
-//
-//   bool _compareEffectiveNames(
-//     FieldDeclaration field,
-//     FormalParameter parameter,
-//   ) {
-//     final relevantField = field.fields.variables.first;
-//
-//     final effectiveFieldName = relevantField.name.lexeme.startsWith('_')
-//         ? relevantField.name.lexeme.substring(1)
-//         : relevantField.name.lexeme;
-//
-//     final effectiveParameterName =
-//         parameter.name?.lexeme.startsWith('_') ?? false
-//         ? parameter.name?.lexeme.substring(1)
-//         : parameter.name?.lexeme;
-//
-//     return effectiveParameterName == effectiveFieldName;
-//   }
-// }
+import 'package:analyzer/analysis_rule/analysis_rule.dart';
+import 'package:analyzer/analysis_rule/rule_context.dart';
+import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/error/error.dart';
+
+/// Displays warning when constructor's parameters' order vary from class
+/// declared fields order. Works for the both named and unnamed parameters.
+class ConstructorParametersAndFieldsShouldHaveTheSameOrder
+    extends AnalysisRule {
+  ConstructorParametersAndFieldsShouldHaveTheSameOrder()
+    : super(
+        name: 'constructor_parameters_and_fields_should_have_the_same_order',
+        description: 'Class parameters and fields should have the same order.',
+      );
+
+  @override
+  LintCode get diagnosticCode => LintCode(name, description);
+
+  @override
+  void registerNodeProcessors(
+    RuleVisitorRegistry registry,
+    RuleContext context,
+  ) {
+    registry.addClassDeclaration(this, _Visitor(this, context));
+  }
+}
+
+class _Visitor extends SimpleAstVisitor<void> {
+  _Visitor(this.rule, this.context);
+
+  final AnalysisRule rule;
+  final RuleContext context;
+
+  @override
+  void visitClassDeclaration(ClassDeclaration node) {
+    final fields = node.members.whereType<FieldDeclaration>().toList();
+
+    if (fields.isEmpty) {
+      return;
+    }
+
+    final constructors = node.members.whereType<ConstructorDeclaration>();
+    for (final constructor in constructors) {
+      if (!_hasValidOrder(constructor, fields)) {
+        rule.reportAtNode(constructor);
+      }
+    }
+  }
+
+  bool _hasValidOrder(
+    ConstructorDeclaration constructor,
+    List<FieldDeclaration> fields,
+  ) {
+    final parameters = constructor.parameters.parameters;
+    if (parameters.isEmpty) {
+      return true;
+    }
+
+    final namedParameters = parameters
+        .where((parameter) => parameter.isNamed && _isNotSuperFormal(parameter))
+        .toList();
+
+    final unnamedParameters = parameters
+        .where(
+          (parameter) => !parameter.isNamed && _isNotSuperFormal(parameter),
+        )
+        .toList();
+
+    final fieldsWithNamedParameters = fields
+        .where(
+          (field) => namedParameters.any(
+            (parameter) => _compareEffectiveNames(field, parameter),
+          ),
+        )
+        .toList();
+
+    final fieldsWithUnnamedParameters = fields
+        .where(
+          (field) => unnamedParameters.any(
+            (parameter) => _compareEffectiveNames(field, parameter),
+          ),
+        )
+        .toList();
+
+    for (
+      var i = 0;
+      i < namedParameters.length && i < fieldsWithNamedParameters.length;
+      i++
+    ) {
+      if (!_compareEffectiveNames(
+        fieldsWithNamedParameters[i],
+        namedParameters[i],
+      )) {
+        return false;
+      }
+    }
+
+    for (
+      var i = 0;
+      i < unnamedParameters.length && i < fieldsWithUnnamedParameters.length;
+      i++
+    ) {
+      if (!_compareEffectiveNames(
+        fieldsWithUnnamedParameters[i],
+        unnamedParameters[i],
+      )) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool _isNotSuperFormal(FormalParameter parameter) =>
+      !(parameter.declaredFragment?.element.isSuperFormal ?? false);
+
+  bool _compareEffectiveNames(
+    FieldDeclaration field,
+    FormalParameter parameter,
+  ) {
+    final relevantField = field.fields.variables.first;
+
+    final effectiveFieldName = relevantField.name.lexeme.startsWith('_')
+        ? relevantField.name.lexeme.substring(1)
+        : relevantField.name.lexeme;
+
+    final effectiveParameterName =
+        parameter.name?.lexeme.startsWith('_') ?? false
+        ? parameter.name?.lexeme.substring(1)
+        : parameter.name?.lexeme;
+
+    return effectiveParameterName == effectiveFieldName;
+  }
+}
