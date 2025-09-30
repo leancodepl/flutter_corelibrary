a6cc6e9a7d843f003538e90854e5e25d7aa8fbfd
Piotr Rogulski <piter.rogulski@gmail.com>
2025-09-30 21:22:26 +0200

    Split test cases for UsePadding

--- packages/leancode_lint/test/use_padding_test.dart
+++ packages/leancode_lint/test/use_padding_test.dart
@@ -26,7 +26,7 @@ class UsePaddingTest extends AnalysisRuleTest {
   @override
   String get analysisRule => rule.name;
 
-  Future<void> test_main() async {
+  Future<void> test_container_with_margin_flagged() async {
     await assertDiagnostics(
       '''
 import 'package:flutter/material.dart';
@@ -36,51 +36,108 @@ void test() {
     margin: const EdgeInsets.all(10),
     child: const SizedBox(),
   );
+}
+''',
+      [lint(57, 9)],
+    );
+  }
 
-  Container(
-    margin: const EdgeInsets.all(10),
-    child: const SizedBox(),
-  );
+  Future<void> test_container_with_margin_and_key_flagged() async {
+    await assertDiagnostics(
+      '''
+import 'package:flutter/material.dart';
 
+void test() {
   Container(
     margin: const EdgeInsets.all(10),
     key: const Key('key'),
     child: const SizedBox(),
   );
+}
+''',
+      [lint(57, 9)],
+    );
+  }
 
+  Future<void> test_container_with_margin_and_color_ignored() async {
+    await assertNoDiagnostics('''
+import 'package:flutter/material.dart';
+
+void test() {
   Container(
     margin: const EdgeInsets.all(10),
     color: Colors.red,
     child: const SizedBox(),
   );
+}
+''');
+  }
 
+  Future<void> test_container_without_margin_ignored() async {
+    await assertNoDiagnostics('''
+import 'package:flutter/material.dart';
+
+void test() {
   Container(
     color: Colors.red,
     child: const SizedBox(),
   );
+}
+''');
+  }
 
+  Future<void> test_container_with_margin_and_no_child_flagged() async {
+    await assertDiagnostics(
+      '''
+import 'package:flutter/material.dart';
+
+void test() {
   Container(
     margin: const EdgeInsets.all(10),
   );
+}
+''',
+      [lint(57, 9)],
+    );
+  }
 
+  Future<void> test_padding_widget_ignored() async {
+    await assertNoDiagnostics('''
+import 'package:flutter/material.dart';
+
+void test() {
   const Padding(
     padding: EdgeInsets.all(10),
     child: SizedBox(),
   );
+}
+''');
+  }
 
+  Future<void> test_container_with_null_margin_ignored() async {
+    await assertNoDiagnostics('''
+import 'package:flutter/material.dart';
+
+void test() {
   Container(
     margin: null,
     child: const SizedBox(),
   );
+}
+''');
+  }
 
+  Future<void> test_container_with_nullable_margin_ignored() async {
+    await assertNoDiagnostics('''
+import 'package:flutter/material.dart';
+
+void test() {
   const EdgeInsets? nullablePadding = null;
   Container(
     margin: nullablePadding,
     child: const SizedBox(),
   );
 }
-''',
-      [lint(57, 9), lint(143, 9), lint(229, 9), lint(522, 9)],
-    );
+''');
   }
 }
