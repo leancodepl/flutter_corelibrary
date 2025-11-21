ebfe554860eb1708e909bfecac51f391083470b1
Piotr Rogulski <piter.rogulski@gmail.com>
2025-11-21 17:35:50 +0100

    Migrate `avoid_single_child_in_multi_child_widgets`

--- packages/leancode_lint/lib/lints/avoid_single_child_in_multi_child_widget.dart
+++ packages/leancode_lint/lib/lints/avoid_single_child_in_multi_child_widget.dart
@@ -1,21 +1,42 @@
+import 'package:analyzer/analysis_rule/analysis_rule.dart';
+import 'package:analyzer/analysis_rule/rule_context.dart';
+import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/error/error.dart' hide LintCode;
-import 'package:analyzer/error/listener.dart';
-import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/error/error.dart';
+import 'package:leancode_lint/type_checker.dart';
 import 'package:leancode_lint/utils.dart';
 
 /// Enforces that some widgets that accept multiple children do not have a single child.
-class AvoidSingleChildInMultiChildWidgets extends DartLintRule {
-  const AvoidSingleChildInMultiChildWidgets()
-    : super(
-        code: const LintCode(
-          name: 'avoid_single_child_in_multi_child_widgets',
-          problemMessage: 'Avoid using {0} with a single child.',
-          correctionMessage:
-              'Remove the {0} and achieve the same result using dedicated widgets.',
-          errorSeverity: DiagnosticSeverity.WARNING,
-        ),
-      );
+class AvoidSingleChildInMultiChildWidgets extends AnalysisRule {
+  AvoidSingleChildInMultiChildWidgets()
+    : super(name: code.name, description: code.problemMessage);
+
+  static const code = LintCode(
+    'avoid_single_child_in_multi_child_widgets',
+    'Avoid using {0} with a single child.',
+    correctionMessage:
+        'Remove the {0} and achieve the same result using dedicated widgets.',
+    severity: .WARNING,
+  );
+
+  @override
+  LintCode get diagnosticCode => code;
+
+  @override
+  void registerNodeProcessors(
+    RuleVisitorRegistry registry,
+    RuleContext context,
+  ) {
+    registry.addInstanceCreationExpression(this, _Visitor(this, context));
+  }
+}
+
+class _Visitor extends SimpleAstVisitor<void> {
+  _Visitor(this.rule, this.context);
+
+  final AnalysisRule rule;
+  final RuleContext context;
 
   static const _complain = [
     ('children', TypeChecker.fromName('Column', packageName: 'flutter')),
@@ -42,47 +63,36 @@ class AvoidSingleChildInMultiChildWidgets extends DartLintRule {
   ];
 
   @override
-  void run(
-    CustomLintResolver resolver,
-    DiagnosticReporter reporter,
-    CustomLintContext context,
-  ) {
-    context.registry.addInstanceCreationExpression((node) {
-      final constructorName = node.constructorName.type;
-      if (constructorName.element case final typeElement?) {
-        // is it something we want to complain about?
-        final match = _complain.firstWhereOrNull(
-          (e) => e.$2.isExactly(typeElement),
-        );
-        if (match == null) {
-          return;
-        }
-
-        // does it have a children argument?
-        var children = node.argumentList.arguments.firstWhereOrNull(
-          (e) => e.correspondingParameter?.name == match.$1,
-        );
-        if (children == null) {
-          return;
-        } else if (children is NamedExpression) {
-          children = children.expression;
-        }
-
-        _checkInstanceCreation(constructorName, children, reporter);
+  void visitInstanceCreationExpression(InstanceCreationExpression node) {
+    final constructorName = node.constructorName.type;
+    if (constructorName.element case final typeElement?) {
+      // is it something we want to complain about?
+      final match = _complain.firstWhereOrNull(
+        (e) => e.$2.isExactly(typeElement),
+      );
+      if (match == null) {
+        return;
       }
-    });
+
+      // does it have a children argument?
+      var children = node.argumentList.arguments.firstWhereOrNull(
+        (e) => e.correspondingParameter?.name == match.$1,
+      );
+      if (children == null) {
+        return;
+      } else if (children is NamedExpression) {
+        children = children.expression;
+      }
+
+      _checkInstanceCreation(constructorName, children);
+    }
   }
 
-  void _checkInstanceCreation(
-    NamedType constructorName,
-    Expression children,
-    DiagnosticReporter reporter,
-  ) {
+  void _checkInstanceCreation(NamedType constructorName, Expression children) {
     if (children case final ListLiteral list) {
       if (_hasSingleElement(list)) {
-        reporter.atNode(
+        rule.reportAtNode(
           constructorName,
-          code,
           arguments: [constructorName.name.lexeme],
         );
       }
