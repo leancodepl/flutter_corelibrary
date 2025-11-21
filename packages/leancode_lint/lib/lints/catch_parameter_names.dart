035ee7e5061c4b4b18237ee4d3dda65a27ef55c5
Piotr Rogulski <piter.rogulski@gmail.com>
2025-11-21 17:41:39 +0100

    Migrate `catch_parameter_names`

--- packages/leancode_lint/lib/lints/catch_parameter_names.dart
+++ packages/leancode_lint/lib/lints/catch_parameter_names.dart
@@ -1,54 +1,59 @@
+import 'package:analyzer/analysis_rule/analysis_rule.dart';
+import 'package:analyzer/analysis_rule/rule_context.dart';
+import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/error/error.dart' hide LintCode;
-import 'package:analyzer/error/listener.dart';
-import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/error/error.dart';
 
 /// Enforces that a catch clause has correctly named variable bindings:
 /// - if it's a catch-all, the exception should be named `err` and the stacktrace `st`
 /// - if it's a typed catch, the stacktrace has to be named `st`
-class CatchParameterNames extends DartLintRule {
-  const CatchParameterNames()
-    : super(
-        code: const LintCode(
-          name: 'catch_parameter_names',
-          problemMessage: 'Parameter name for the {0} is non-standard.',
-          correctionMessage: 'Rename the parameter to {1}`.',
-          errorSeverity: DiagnosticSeverity.WARNING,
-        ),
-      );
+class CatchParameterNames extends AnalysisRule {
+  CatchParameterNames()
+    : super(name: code.name, description: code.problemMessage);
+
+  static const code = LintCode(
+    'catch_parameter_names',
+    'Parameter name for the {0} is non-standard.',
+    correctionMessage: 'Rename the parameter to {1}`.',
+    severity: .WARNING,
+  );
 
   @override
-  void run(
-    CustomLintResolver resolver,
-    DiagnosticReporter reporter,
-    CustomLintContext context,
-  ) {
-    context.registry.addCatchClause((node) {
-      // not a typed catch `} on TypeName {`
-      if (node.exceptionType == null) {
-        _checkParameter(
-          node.exceptionParameter,
-          _CatchClauseParameter.exception,
-          reporter,
-        );
-      }
+  LintCode get diagnosticCode => code;
 
-      _checkParameter(
-        node.stackTraceParameter,
-        _CatchClauseParameter.stackTrace,
-        reporter,
-      );
-    });
+  @override
+  void registerNodeProcessors(
+    RuleVisitorRegistry registry,
+    RuleContext context,
+  ) {
+    registry.addCatchClause(this, _Visitor(this, context));
+  }
+}
+
+class _Visitor extends SimpleAstVisitor<void> {
+  _Visitor(this.rule, this.context);
+
+  final AnalysisRule rule;
+  final RuleContext context;
+
+  @override
+  void visitCatchClause(CatchClause node) {
+    // not a typed catch `} on TypeName {`
+    if (node.exceptionType == null) {
+      _checkParameter(node.exceptionParameter, .exception);
+    }
+
+    _checkParameter(node.stackTraceParameter, .stackTrace);
   }
 
   void _checkParameter(
     CatchClauseParameter? node,
     _CatchClauseParameter param,
-    DiagnosticReporter reporter,
   ) {
     if (node != null &&
         !{'_', param.preferredName}.contains(node.name.lexeme)) {
-      reporter.atNode(node, code, arguments: [param.name, param.preferredName]);
+      rule.reportAtNode(node, arguments: [param.name, param.preferredName]);
     }
   }
 }
