4c441b8ba70bac8282554c456c0e62073fa341d4
Piotr Rogulski <piter.rogulski@gmail.com>
2025-11-21 19:32:46 +0100

    Migrate plugin's main file

--- packages/leancode_lint/lib/leancode_lint.dart
+++ packages/leancode_lint/lib/leancode_lint.dart
@@ -1,14 +1,15 @@
-import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:analysis_server_plugin/plugin.dart';
+import 'package:analysis_server_plugin/registry.dart';
 import 'package:leancode_lint/assists/convert_iterable_map_to_collection_for.dart';
 import 'package:leancode_lint/assists/convert_positional_to_named_formal.dart';
 import 'package:leancode_lint/assists/convert_record_into_nominal_type.dart';
+import 'package:leancode_lint/helpers.dart';
 import 'package:leancode_lint/lints/add_cubit_suffix_for_cubits.dart';
 import 'package:leancode_lint/lints/avoid_conditional_hooks.dart';
 import 'package:leancode_lint/lints/avoid_single_child_in_multi_child_widget.dart';
 import 'package:leancode_lint/lints/catch_parameter_names.dart';
 import 'package:leancode_lint/lints/constructor_parameters_and_fields_should_have_the_same_order.dart';
 import 'package:leancode_lint/lints/hook_widget_does_not_use_hooks.dart';
-import 'package:leancode_lint/lints/prefer_center_over_align.dart';
 import 'package:leancode_lint/lints/prefix_widgets_returning_slivers.dart';
 import 'package:leancode_lint/lints/start_comments_with_space.dart';
 import 'package:leancode_lint/lints/use_align.dart';
@@ -16,30 +17,48 @@ import 'package:leancode_lint/lints/use_dedicated_media_query_methods.dart';
 import 'package:leancode_lint/lints/use_design_system_item.dart';
 import 'package:leancode_lint/lints/use_padding.dart';
 
-PluginBase createPlugin() => _Linter();
+final plugin = _Linter();
 
-class _Linter extends PluginBase {
+class _Linter extends Plugin {
   @override
-  List<LintRule> getLintRules(CustomLintConfigs configs) => [
-    const StartCommentsWithSpace(),
-    ...UseDesignSystemItem.getRulesListFromConfigs(configs),
-    PrefixWidgetsReturningSlivers.fromConfigs(configs),
-    const AddCubitSuffixForYourCubits(),
-    const CatchParameterNames(),
-    const AvoidConditionalHooks(),
-    const HookWidgetDoesNotUseHooks(),
-    const ConstructorParametersAndFieldsShouldHaveTheSameOrder(),
-    const AvoidSingleChildInMultiChildWidgets(),
-    const UseAlign(),
-    const UsePadding(),
-    const UseDedicatedMediaQueryMethods(),
-    const PreferCenterOverAlign(),
-  ];
+  String get name => 'leancode_lint';
 
   @override
-  List<Assist> getAssists() => [
-    ConvertRecordIntoNominalType(),
-    ConvertPositionalToNamedFormal(),
-    ConvertIterableMapToCollectionFor(),
-  ];
+  void register(PluginRegistry registry) {
+    registry
+      ..registerWarningRule(StartCommentsWithSpace())
+      ..registerWarningRule(UseDesignSystemItem())
+      ..registerWarningRule(PrefixWidgetsReturningSlivers())
+      ..registerFixForRule(
+        StartCommentsWithSpace.code,
+        AddStartingSpaceToComment.new,
+      )
+      ..registerWarningRule(AddCubitSuffixForYourCubits())
+      ..registerWarningRule(CatchParameterNames())
+      ..registerWarningRule(AvoidConditionalHooks())
+      ..registerWarningRule(HookWidgetDoesNotUseHooks())
+      ..registerFixForRule(
+        HookWidgetDoesNotUseHooks.code,
+        ConvertHookWidgetToStatelessWidget.new,
+      )
+      // TODO: disabled by default until stabilized. Add documentation.
+      ..registerLintRule(ConstructorParametersAndFieldsShouldHaveTheSameOrder())
+      ..registerWarningRule(AvoidSingleChildInMultiChildWidgets())
+      ..registerWarningRule(UseAlign())
+      ..registerFixForRule(
+        UseAlign.code,
+        ChangeWidgetNameFix.producerGeneratorFor('Align'),
+      )
+      ..registerWarningRule(UsePadding())
+      ..registerFixForRule(UsePadding.code, UsePaddingFix.new)
+      ..registerWarningRule(UseDedicatedMediaQueryMethods())
+      ..registerFixForRule(
+        UseDedicatedMediaQueryMethods.code,
+        ReplaceMediaQueryOfWithDedicatedMethodFix.new,
+      )
+      // ..registerAssist(PreferCenterOverAlign())
+      ..registerAssist(ConvertRecordIntoNominalType.new)
+      ..registerAssist(ConvertPositionalToNamedFormal.new)
+      ..registerAssist(ConvertIterableMapToCollectionFor.new);
+  }
 }
