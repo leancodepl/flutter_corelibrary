0df09840d6418891be2ac41ad8946991ba9721dd
Łukasz Komoszyński <lukasz.komoszynski@leancode.pl>
2026-09-14 09:51:04 +0200

    Simplify ClassName constructors
    Co-authored-by: Piotr Rogulski <piotr.rogulski@leancode.pl>

--- packages/jaspr_class_scope/lib/src/class_name.dart
+++ packages/jaspr_class_scope/lib/src/class_name.dart
@@ -5,16 +5,13 @@ import 'package:jaspr_class_scope/src/class_scope.dart';
 final class ClassName {
   /// A class rendered as written, because something outside the component
   /// knows it by [name].
-  const ClassName.shared(String name)
-    : _local = name,
-      _scope = null,
+  const ClassName.shared(this._local)
+    : _scope = null,
       _and = null;
 
   /// The class [local] of [scope]'s component.
-  const ClassName.scoped(String local, ClassScope scope)
-    : _local = local,
-      _scope = scope,
-      _and = null;
+  const ClassName.scoped(this._local, this._scope)
+    : _and = null;
 
   const ClassName._(this._local, this._scope, this._and);
 
